f80ce8860f3af938518d71802cd3dbabee834fcd
Devhadrao <gauri-ramesh.devhadrao@capgemini.com>
2023-01-08 18:01:24 +0530

    Assignment 4

---
+++ Assignment_7_Q4/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace Assignment_7_Q4
+{
+    [Serializable]
+    class Employee
+    {
+        public int id;
+        public string name;
+        public double salary;
+
+        public Employee(int id, string name, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+    }
+}
---
+++ Assignment_7_Q4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Assignment_7_Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\Users\GDEVHADR\source\repos\Cloud-\Assignment_7_Q2\Output.txt";
+            Employee emp = new Employee(101, "Gauri", 70000);
+            FileStream stream = new FileStream(file, FileMode.OpenOrCreate);
+            BinaryFormatter bf = new BinaryFormatter();
+
+
+            bf.Serialize(stream, emp);
+            stream.Close();
+            Console.WriteLine(" File created successfully -> " + file);
+
+
+            FileStream stream1 = new FileStream(file, FileMode.Open, FileAccess.Read);
+            Employee objnew = (Employee)bf.Deserialize(stream1);
+
+            Console.WriteLine(objnew.id);
+            Console.WriteLine(objnew.name);
+            Console.WriteLine(objnew.salary);
+        }
+    }
+}
