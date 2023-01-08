7414a75d46aab028dde756e57927625a2a61148c
Devhadrao <gauri-ramesh.devhadrao@capgemini.com>
2023-01-08 17:33:24 +0530

    Assignment 1

---
+++ Assignment_7_Q1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment_7_Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\GDEVHADR\project_dir";
+
+            var dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            var files = Directory.GetFiles(rootPath, "*.txt", SearchOption.AllDirectories);
+
+            Console.WriteLine("******Directories*******");
+            Console.Write("\n");
+            foreach (string dir in dirs)
+            {
+                var dirInfo = new DirectoryInfo(dir);
+                Console.WriteLine($"{dirInfo.FullName}");
+            }
+            Console.WriteLine("\n*******All .txt files*******");
+            Console.Write("\n");
+            foreach (string file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                Console.WriteLine($"{Path.GetFileName(file)} - { fileInfo.Length } bytes");
+            }
+        }
+    }
+}
