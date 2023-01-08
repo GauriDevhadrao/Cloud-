6f5efbcdffcf4102bd8a4c98aed1471315af39e0
Devhadrao <gauri-ramesh.devhadrao@capgemini.com>
2023-01-08 18:17:08 +0530

    Assignment 3 & 7-3 Combined

---
+++ Assignment 3/Employee.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Assignment_3
+{
+    interface IPrintable
+    {
+        public void DisplayEmpDetails();
+
+    }
+    [Serializable]
+    class Employee : IPrintable
+    {
+        private int EmpNo;
+        private string EmpName;
+        private double Salary;
+        private double hra;
+        private double ta;
+        private double da;
+        private double pf;
+        private double tds;
+        private double NetSalary;
+        private double GrossSalary;
+        public int EMPNO { get; set; }
+        public string EMPNAME { get; set; }
+        public double SALARY { get; set; }
+        public double HRA { get; set; }
+        public double TA { get; set; }
+        public double DA { get; set; }
+        public double PF { get; set; }
+        public double TDS { get; set; }
+        public double NETSALARY { get; set; }
+        public double GROSSSALARY { get; set; }
+
+        public Employee()
+        //int EmpNo, string EmpName, double Salary, double hra, double ta, double da, double pf, double tds, double NetSalary, double GrossSalary
+        {
+            this.EMPNO = EmpNo;
+            this.EMPNAME = EmpName;
+            this.SALARY = Salary;
+            this.HRA = hra;
+            this.TA = ta;
+            this.DA = da;
+            this.PF = pf;
+            this.TDS = tds;
+            this.NETSALARY = NetSalary;
+            this.GROSSSALARY = GrossSalary;
+        }
+        public virtual void CalculateDetails()
+        {
+            if (SALARY < 5000)
+            {
+                HRA = (10 * SALARY) / 100;
+                TA = (5 * SALARY) / 100;
+                DA = (15 * SALARY) / 100;
+                GROSSSALARY = SALARY + HRA + TA + DA;
+                PF = (10 * GROSSSALARY) / 100;
+                TDS = (18 * GROSSSALARY) / 100;
+                NETSALARY = GROSSSALARY - (PF + TDS);
+
+
+            }
+            else if (SALARY < 10000)
+            {
+                HRA = (15 * SALARY) / 100;
+                TA = (10 * SALARY) / 100;
+                DA = (20 * SALARY) / 100;
+                GROSSSALARY = SALARY + HRA + TA + DA;
+                PF = (10 * GROSSSALARY) / 100;
+                TDS = (18 * GROSSSALARY) / 100;
+                NETSALARY = GROSSSALARY - (PF + TDS);
+            }
+            else if (SALARY < 15000)
+            {
+                HRA = (20 * SALARY) / 100;
+                TA = (15 * SALARY) / 100;
+                DA = (25 * SALARY) / 100;
+                GROSSSALARY = SALARY + HRA + TA + DA;
+                PF = (10 * GROSSSALARY) / 100;
+                TDS = (18 * GROSSSALARY) / 100;
+                NETSALARY = GROSSSALARY - (PF + TDS);
+            }
+            else if (SALARY < 20000)
+            {
+                HRA = (25 * SALARY) / 100;
+                TA = (20 * SALARY) / 100;
+                DA = (30 * SALARY) / 100;
+                GROSSSALARY = SALARY + HRA + TA + DA;
+                PF = (10 * GROSSSALARY) / 100;
+                TDS = (18 * GROSSSALARY) / 100;
+                NETSALARY = GROSSSALARY - (PF + TDS);
+            }
+            else if (SALARY >= 20000)
+            {
+                HRA = (30 * SALARY) / 100;
+                TA = (25 * SALARY) / 100;
+                DA = (35 * SALARY) / 100;
+                GROSSSALARY = SALARY + HRA + TA + DA;
+                PF = (10 * GROSSSALARY) / 100;
+                TDS = (18 * GROSSSALARY) / 100;
+                NETSALARY = GROSSSALARY - (PF + TDS);
+            }
+
+        }
+        public void DisplayEmpDetails()
+        {
+            Console.WriteLine("The Emp No is " + EMPNO);
+            Console.WriteLine("The Emp Name is " + EMPNAME);
+            Console.WriteLine("The Emp Salary is " + SALARY);
+            Console.WriteLine("The Emp HRA is " + HRA);
+            Console.WriteLine("The Emp TA is " + TA);
+            Console.WriteLine("The Emp DA is " + DA);
+            Console.WriteLine("The Emp PF is " + PF);
+            Console.WriteLine("The Emp TDS is " + TDS);
+            Console.WriteLine("The Emp Gross Salary is " + GROSSSALARY);
+            Console.WriteLine("The Emp Net Salary is " + NETSALARY);
+
+            //Console.WriteLine("The Emp Id is  " + EMPNO + " The Emp Name is " + EMPNAME + " The Emp Salary is " + SALARY + " The Emp HRA is " + HRA + " The Emp TA is " + TA + " The Emp DA is " + DA + " The Emp PF is " + PF + " The Emp TDS is " + TDS + " The Emp Netsalary is " + NETSALARY + " The Emp gross salary is " + GROSSSALARY);
+        }
+    }
+    [Serializable]
+    class Manager : Employee, IPrintable
+    {
+        private double petrolAllowance;
+        private double foodAllowance;
+        private double otherAllowance;
+
+        public double PetrolAllowance { get; set; }
+        public double FoodAllowance { get; set; }
+        public double OtherAllowance { get; set; }
+        public Manager()
+        {
+            this.PetrolAllowance = petrolAllowance;
+            this.FoodAllowance = foodAllowance;
+            this.OtherAllowance = otherAllowance;
+        }
+        public new void CalculateSalary()
+        {
+            PetrolAllowance = (8 * SALARY) / 100;
+            FoodAllowance = (13 * SALARY) / 100;
+            OtherAllowance = (3 * SALARY) / 100;
+            base.CalculateDetails();
+            GROSSSALARY = PetrolAllowance + FoodAllowance + OtherAllowance;
+        }
+        public override void CalculateDetails()
+        {
+            double GS = GROSSSALARY;
+            PF = (10 * GS) / 100;
+            TDS = (18 * GS) / 100;
+            NETSALARY = GS - (PF + TDS);
+        }
+        public void DisplayManDetails()
+        {
+            Console.WriteLine("Petrol Allowance is " + PetrolAllowance);
+            Console.WriteLine("Food Allowance is " + FoodAllowance);
+            Console.WriteLine("Other Allowance is " + OtherAllowance);
+            Console.WriteLine("Gross Salary is " + GROSSSALARY);
+            Console.WriteLine("PF of manager is " + PF);
+            Console.WriteLine("TDS of manager is " + TDS);
+            Console.WriteLine("Net Salary of manager is " + NETSALARY);
+        }
+    }
+    [Serializable]
+    class MarketingExecutive : Employee, IPrintable
+    {
+        double kmsTravel;
+        double tourAllowance;
+        double TeleAllowance;
+        double GS;
+        public void CalculteMarketing()
+        {
+            Console.WriteLine("Enter Kilometer Travel: ");
+            kmsTravel = Convert.ToDouble(Console.ReadLine());
+            tourAllowance = 5 * kmsTravel;
+            TeleAllowance = 1000;
+            base.CalculateDetails();
+            GROSSSALARY = kmsTravel + tourAllowance + TeleAllowance;
+            GS = GROSSSALARY;
+        }
+        public override void CalculateDetails()
+        {
+            PF = (10 * GS) / 100;
+            TDS = (18 * GS) / 100;
+            NETSALARY = GS - (PF + TDS);
+        }
+        public void DisplayMarketing()
+        {
+            Console.WriteLine("Tour Allowance: " + tourAllowance);
+            Console.WriteLine("Kilometers Travel: " + kmsTravel);
+            Console.WriteLine("Gross Salary is " + GROSSSALARY);
+            Console.WriteLine("PF of marketing executive is " + PF);
+            Console.WriteLine("TDS of marketing executive is " + TDS);
+            Console.WriteLine("Net Salary of marketing executive is " + NETSALARY);
+            Console.ReadKey();
+        }
+    }
+}
--- Assignment 3/Program.cs
+++ Assignment 3/Program.cs
@@ -1,197 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
-namespace Assignment_3OOP_Q1
+namespace OOP_Assignment_3
 {
-    class Employee
+    class Program
     {
-        private int EmpNo;
-        private string EmpName;
-        private double Salary;
-        private double HRA;
-        private double TA;
-        private double DA;
-        private double PF;
-        private double TDS;
-        private double NetSalary;
-        private double GrossSalary;
 
-        public int empno { get; set; }
-        public string empname { get; set; }
-        public double salary { get; set; }
-        public double hra { get; set; }
-        public double ta { get; set; }
-        public double da { get; set; }
-        public double pf { get; set; }
-        public double tds { get; set; }
-        public double netsalary { get; set; }
-        public double grosssalary { get; set; }
-        public Employee()
-        //(int EmpNo, string EmpName, double Salary, double HRA, double TA, double DA, double PF, double TDS, double NetSalary, double GrossSalary)
+        public static void SerializeObject(Object o)
         {
-            this.empno = EmpNo;
-            this.empname = EmpName;
-            this.salary = Salary;
-            this.hra = HRA;
-            this.ta = TA;
-            this.da = DA;
-            this.pf = PF;
-            this.tds = TDS;
-            this.netsalary = NetSalary;
-            this.grosssalary = GrossSalary;
+            FileStream fs = new FileStream(@"C:\Github\.Net-FullStack\c#\OOP_Assignment_3\Output.txt", FileMode.OpenOrCreate);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fs, o);
+            fs.Close();
         }
-        //Member fnctions
-        public void DisplayEmpDetails()
-        {
-            Console.WriteLine("The Emp No is " + empno + " The Emp Name is " + empname + " The Salary is " + salary + "The HRA is " + hra + "The TA is " + ta + "The DA is " + da + "The PF is " + pf + "The TDS is " + tds + "The NetSalary is " + netsalary + "The GrossSalary is " + grosssalary);
 
+        public static void DeserializeObject()
+        {
+            FileStream fs = new FileStream(@"C:\Github\.Net-FullStack\c#\OOP_Assignment_3\Output.txt", FileMode.OpenOrCreate);
+            BinaryFormatter formatter = new BinaryFormatter();
+            Manager m1 = new Manager();
+            m1 = (Manager)formatter.Deserialize(fs);
+            Console.WriteLine($"-----Printing DeSerialized Manager Object-----\n{m1}");
+            fs.Close();
         }
-        public virtual void CalculateEmpDetails()
+
+        public static void Main(string[] args)
         {
-            if (salary < 5000)
-            {
-                hra = (salary * 10) / 100;
-                ta = (salary * 5) / 100;
-                da = (salary * 15) / 100;
-                grosssalary = salary + hra + ta + da;
-                pf = (grosssalary * 10) / 100;
-                tds = (grosssalary * 18) / 100;
-                netsalary = grosssalary - (pf + tds);
-            }
-            else if (salary < 10000)
-            {
-                hra = (salary * 10) / 100;
-                ta = (salary * 5) / 100;
-                da = (salary * 15) / 100;
-                grosssalary = salary + hra + ta + da;
-                pf = (grosssalary * 10) / 100;
-                tds = (grosssalary * 18) / 100;
-                netsalary = grosssalary - (pf + tds);
-            }
-            else if (salary < 15000)
-            {
-                hra = (salary * 20) / 100;
-                ta = (salary * 15) / 100;
-                da = (salary * 25) / 100;
-                grosssalary = salary + hra + ta + da;
-                pf = (grosssalary * 10) / 100;
-                tds = (grosssalary * 18) / 100;
-                netsalary = grosssalary - (pf + tds);
-            }
-            else if (salary < 20000)
-            {
-                hra = (salary * 25) / 100;
-                ta = (salary * 20) / 100;
-                da = (salary * 30) / 100;
-                grosssalary = salary + hra + ta + da;
-                pf = (grosssalary * 10) / 100;
-                tds = (grosssalary * 18) / 100;
-                netsalary = grosssalary - (pf + tds);
-            }
-            else if (salary >= 20000)
-            {
-                hra = (salary * 30) / 100;
-                ta = (salary * 25) / 100;
-                da = (salary * 35) / 100;
-                grosssalary = salary + hra + ta + da;
-                pf = (grosssalary * 10) / 100;
-                tds = (grosssalary * 18) / 100;
-                netsalary = grosssalary - (pf + tds);
-            }
-        }
-        class Manager: Employee
-        {
-            double Petrol;
-            double Food;
-            double Other;
-            double gs;
-            public new void CalculateGross()
-            {
-                Petrol = (salary * 8) /100;
-                Food = (salary * 13) / 100;
-                Other = (salary * 3) / 100;
-                base.CalculateEmpDetails();
-                grosssalary = grosssalary + Petrol + Food + Other;
-            }
-            public override void CalculateEmpDetails()
-            {
-                pf = (grosssalary * 10) / 100;
-                tds = (grosssalary * 18) /100;
-                netsalary = netsalary = grosssalary - (pf + tds);
+            //Console.WriteLine("Hello World!");
+            Manager emp = new Manager();
+            MarketingExecutive M = new MarketingExecutive();
+            Console.WriteLine("Enter Employee Number: ");
+            emp.EMPNO = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee name: ");
+            emp.EMPNAME = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter Employee Salary: ");
+            emp.SALARY = Convert.ToDouble(Console.ReadLine());
+            //emp.CalculateDetails();
+            //emp.DisplayEmpDetails();
+            emp.CalculateSalary();
+            //Serializing and DeSerializing from assignment-7
+            SerializeObject(emp);
+            DeserializeObject();
 
-            }
-            public new void Display()
+
+            emp.CalculateDetails();
+            emp.DisplayManDetails();
+            M.CalculteMarketing();
+            M.CalculateDetails();
+            M.DisplayMarketing();
+
+            StreamReader s = new StreamReader(@"C:\Github\.Net-FullStack\c#\OOP_Assignment_3\Output.txt");
+            string line = "";
+            while ((line = s.ReadLine()) != null)
             {
-            Console.WriteLine("\n The Emp No is " + empno + "\n  The Emp Name is " + empname + " \n The Salary is " + salary + "\n  The Petrol allowance  is " + Petrol + "\n  The Food allowance is " + Food + "\n  The Other allowance is " + Other + "\n The HRA is " + hra + "\n The TA is " + ta + "\n The DA is " + da + "\n The PF is " + pf + "\n The TDS is " + tds + "\n The NetSalary is " + netsalary + " \n The GrossSalary is " + grosssalary);
-
+                Console.WriteLine(line);
             }
-        }
-        class MarketingExecutive:Employee
-        {
-            double KmTravel;
-            double Tel;
-            double Tour;
-            double gs;
+            s.Close();
 
-            public new void CalculateGross()
-            {
-                Console.WriteLine("Enter Travel Distance in Km");
-                KmTravel = Convert.ToInt32(Console.ReadLine());
-                Tel = 1000;
-                Tour = KmTravel *5;
-                base.CalculateEmpDetails();
-                gs = gs + Tel + Tour ;
-
-            }
-            public override void CalculateEmpDetails()
-            {
-                pf = (grosssalary * 10) / 100;
-                tds = (grosssalary * 18) / 100;
-                netsalary = netsalary = grosssalary - (pf + tds);
-
-            }
-            public new void DisplayMarketing()
-            {
-                Console.WriteLine(  "\n  The Telephone allowance  is " + Tel + "\n  The Tour allowance  is " + Tour );
-
-            }
-
-        }
-        public static void Main(String[] args)
-        {
-            Manager A = new Manager();
-            MarketingExecutive B = new MarketingExecutive();
-            Console.WriteLine("\n Enter Employee No");
-            A.empno =
-            Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n Enter Employee Name");
-            A.empname =
-            Convert.ToString(Console.ReadLine());
-            Console.WriteLine("\n Enter Employee Salary");
-            A.salary =
-            Convert.ToDouble(Console.ReadLine());
-
-            A.CalculateGross();
-            A.CalculateEmpDetails();
-            A.Display();
-            
-            Console.WriteLine("\n Enter Employee No");
-            B.empno =
-            Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n Enter Employee Name");
-            B.empname =
-            Convert.ToString(Console.ReadLine());
-            Console.WriteLine("\n Enter Employee Salary");
-            B.salary =
-            Convert.ToDouble(Console.ReadLine());
-
-            B.CalculateGross();
-            B.CalculateEmpDetails();
-            B.DisplayMarketing();
             Console.ReadKey();
         }
     }
 }
 
-    
